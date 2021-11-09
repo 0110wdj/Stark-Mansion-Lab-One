b8304191e60a8113f4b3ce98690ecce871d87f37
snofly <snofly0110@gmail.com>
2021-11-09 22:43:49 +0800

    snofly:2048网页游戏 +: 增加游戏盘的初始化显示 *: 修改代码格式

--- webGames/2048/My2048.js
+++ webGames/2048/My2048.js
@@ -4,8 +4,6 @@
       len = opt.len,
       size = opt.size,
       margin = opt.margin;
-    var view = new View(prefix, len, size, margin);
-    // console.log(prefix, len, size, margin);
 
     // 设置棋盘背景的宽度和高度
     function View() {
@@ -24,6 +22,29 @@
       this.container.css({ width: containerSize, height: containerSize });
       this.nums = {};
     }
+    View.prototype = {
+      getPos: function (n) {
+        return this.margin + n * (this.size + this.margin);
+      },
+      init: function () {
+        for (var x = 0, len = this.len; x < len; ++x) {
+          for (var y = 0; y < len; ++y) {
+            var $cell = $('<div class="' + this.prefix + '-cell"></div>');
+            $cell.css({
+              width: this.size + 'px',
+              height: this.size + 'px',
+              top: this.getPos(x),
+              left: this.getPos(y),
+            }).appendTo(this.container);
+          }
+        }
+      },
+    };
+    var view = new View(prefix, len, size, margin);
+    view.init();
+    // console.log(prefix, len, size, margin);
+
+
   }
   window["My2048"] = My2048;
 })(window, document, jQuery); //jQuery 区分大小写
