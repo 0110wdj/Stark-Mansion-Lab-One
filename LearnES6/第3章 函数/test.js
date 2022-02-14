5d3f743afb13620d1cd038c088c7316c712eea03
冰上飞熊 <snofly0110@gmail.com>
2022-02-14 14:55:02 +0800

    冰上飞熊: 了解箭头函数等 *: 增加测试代码

--- LearnES6/第3章 函数/test.js
+++ LearnES6/第3章 函数/test.js
@@ -30,6 +30,67 @@ pick({ 'a': 1, 'b': 2 }, 'a', 'b', 'c');
 console.log('pick2.length:' + pick2.length);
 console.log('pick3.length:' + pick3.length); */
 
-let values = [1, 2, 3, 4, 5]
+/* let values = [1, 2, 3, 4, 5]
 console.log(Math.max(...values));
-console.log(Math.max(1, 2, 3));
+console.log(Math.max(1, 2, 3)); */
+
+// function Person(name) {
+//     if (this instanceof Person) {
+//         this.name = name;
+//         console.log('inside');
+//     } /* else {
+//         throw new Error('invoke Person by newKey')
+//     } */
+// }
+
+// var person = new Person('Nicholas');
+// var notAPerson = Person('Nicholas');
+// var notAPersonCall = Person.call(person, 'Nicholas');
+
+// console.log(person);
+// console.log(notAPerson);
+// console.log(notAPersonCall);
+
+// function pp(params) {
+//     console.log(new.target);
+// }
+// var person = new pp('Nicholas');
+// var person = pp.call(this, 'Nicholas');
+
+// if (true) {
+//     console.log(typeof test);
+//     function test(params) {
+//         console.log('test');
+//     }
+//     test();
+// }
+// console.log(typeof test);
+
+/* let test = value => value;
+console.log(test('bala'));
+let sum = (num1, num2) => num1 + num2;
+console.log(sum(2, 5));
+let getName = () => 'Nicholas';
+console.log(getName()); */
+
+/* let person = function (name) {
+    return {
+        getName: function () {
+            return name;
+        }
+    }
+}("Nicholas");
+console.log(person.getName()); */
+
+/* let person = ((name) => {
+    return {
+        getName: function () {
+            return name;
+        }
+    }
+})('Nicholas');
+console.log(person.getName()); */
+
+var values = [3, 2, 1];
+var result = values.sort((a, b) => a - b);
+console.log(result);
