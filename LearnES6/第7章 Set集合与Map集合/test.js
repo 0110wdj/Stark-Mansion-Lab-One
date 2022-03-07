62b5e88e898d8f4399ebd9d98d0bd2dc6fb2df06
冰上飞熊 <snofly0110@gmail.com>
2022-03-07 20:50:52 +0800

    冰上飞熊：了解set与map集合 +：增加测试代码

--- LearnES6/第7章 Set集合与Map集合/test.js
+++ LearnES6/第7章 Set集合与Map集合/test.js
@@ -1,1 +1,30 @@
 // 新增文件
+/* var testObject = {}
+var otherObject = {}
+
+console.log(testObject);
+console.log(otherObject);
+
+var map = Object.create(null);
+map[testObject] = 'foo';
+
+console.log(map);
+console.log(map[testObject]);
+console.log(map[otherObject]);
+console.log(map['[object Object]']);
+
+var otherObjectWithContext = {'a':'1'}
+console.log(otherObjectWithContext);
+map[otherObjectWithContext] = 'otherObjectWithContext';
+console.log(map[otherObjectWithContext]);
+console.log(map['[object Object]']); */
+
+let map = new Map(),
+    key1 = {},
+    key2 = {};
+
+map.set(key1, 5);
+map.set(key2, 25);
+
+console.log(map);
+console.log(map.get({}));
