0968d5435722a94da060f4ba12c8c60926f7e9be
Snofly <626796235@qq.com>
2022-04-01 23:13:50 +0800

    冰上飞熊：了解ES6中类的机制 +：增加测试代码

---
+++ LearnES6/第9章 JavaScript中的类/test.js
@@ -0,0 +1,39 @@
+/* function PersonType(name) {
+    this.name = name;
+}
+PersonType.prototype.sayName = function () {
+    console.log(this.name);
+}
+var person = new PersonType('test')
+person.sayName()
+
+console.log(person instanceof PersonType);
+console.log(person instanceof Object); */
+
+/* class PersonType {
+    constructor(name) {
+        this.name = name;
+    }
+    sayName() {
+        console.log(this.name);
+    }
+}
+let person = new PersonType('test');
+person.sayName();
+
+PersonType.prototype.sayNameAdd = function testAdd() {
+    console.log('test add');
+}
+PersonType.prototype.sayNameAddValue = 2;
+
+// person.sayNameAdd();
+// console.log(person.name);
+// console.log(person.sayNameAddValue);
+// console.log(person.prototype.name = 3);
+// console.log(person.prototype = 'test change');
+// console.log(person.prototype);
+// console.log(person.name = 2);
+// console.log(person.prototype = { a: 1, b: 2 });
+// console.log(person.prototype.a = 3);
+// console.log(person.prototype); */
+
