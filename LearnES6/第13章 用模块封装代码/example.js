ec1819215e9126db02c342d029b9877a1e2dd963
snofly <626796235@qq.com>
2022-04-25 19:33:53 +0800

    snofly: module test

---
+++ LearnES6/第13章 用模块封装代码/example.js
@@ -0,0 +1,9 @@
+console.log('----import mutil');
+
+export function sum(a, b) {
+    return a + b;
+}
+
+export function mutil(a, b) {
+    return a * b;
+}
---
+++ LearnES6/第13章 用模块封装代码/test.js
@@ -0,0 +1,8 @@
+/* import { sum } from "./example.js"
+import { mutil } from "./example.js"
+
+console.log(sum(2, 5));
+console.log(mutil(2, 5)); */
+
+
+
