1bf3a830d66b9821704cd65ef8fa3a68c9d3201e
snofly <626796235@qq.com>
2022-07-13 22:31:39 +0800

    test

---
+++ YouDontknowJSVolumeOne/第2部分 this和对象原型/第2章 this 全面解析/test.js
@@ -0,0 +1,32 @@
+/*
+ * @description: 
+ * @RGX/RBX: 
+ * @Authored: LiuJie
+ * @Date: 2022-07-13 22:03:34
+ * @LastEditors: LiuJie
+ * @LastEditTime: 2022-07-13 22:28:47
+ */
+/* function foo() {
+  console.log(this.a)
+}
+
+var a = 2 
+var obj = {
+  a: 3,
+  foo: foo
+}
+
+foo.call(null)
+foo()
+foo.call()
+foo.call(obj)
+foo.call(obj, 1)
+obj.foo() */
+
+function foo(a, b) {
+  console.log('a: ' + a, 'b: ' + b);
+}
+
+foo.apply(null, [1, 2]);
+var bar = foo.bind(null, 1);
+bar(2);
