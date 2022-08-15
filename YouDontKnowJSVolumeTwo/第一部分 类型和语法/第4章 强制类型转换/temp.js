47402b7801eead9626985d979a8a06232162c32c
Snofly <626796235@qq.com>
2022-08-15 15:15:57 +0800

    测试

---
+++ YouDontKnowJSVolumeTwo/第一部分 类型和语法/第4章 强制类型转换/temp.js
@@ -0,0 +1,30 @@
+// var a = { c: 1 }
+// console.log(JSON.stringify(a));
+// // console.log(JSON.stringify(a.toJSON()));
+// a.toJSON = () => {
+//   return { d: 2 }
+// }
+// console.log(JSON.stringify(a));
+
+// console.log(Math.floor(49.6));
+// console.log(Math.floor(-49.6));
+// console.log(~~(-49.6));
+// console.log(~(-49.6));
+
+// console.log(parseInt(1 / 0, 19));
+// console.log(parseInt(0.000008));
+// console.log(parseInt(0.0000008));
+// console.log(parseInt(false, 16));
+// console.log(parseInt(parseInt, 16));
+
+// console.log(parseInt("0x10"));
+// console.log(parseInt("103", 2));
+
+// var a = [1, 2]
+// var b = [1, 2]
+// console.log(+a);
+// console.log(+b);
+// console.log(a - b);
+// var c = [3]
+// var d = [2]
+// console.log(c - d);
