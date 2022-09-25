8f4b17fc0f564e1f062c86696f94cfb44b72ecfc
Snofly <626796235@qq.com>
2022-09-25 17:27:38 +0800

    生成器与Promise

--- YouDontKnowJSVolumeTwo/第二部分 异步和性能/第4章 生成器/test.js
+++ YouDontKnowJSVolumeTwo/第二部分 异步和性能/第4章 生成器/test.js
@@ -1,27 +1,46 @@
-/*
- * @description: 
- * @RGX/RBX: 
- * @Authored: LiuJie
- * @Date: 2022-09-24 19:47:12
- * @LastEditors: LiuJie
- * @LastEditTime: 2022-09-24 19:50:22
- */
-var x = 1;
-function* foo() {
-  x++;
-  yield;
-  console.log('x:', x);
-}
-function bar() {
-  x++;
-}
+// var x = 1;
+// function* foo() {
+//   x++;
+//   yield;
+//   console.log('x:', x);
+// }
+// function bar() {
+//   x++;
+// }
 
-var it = foo()
+// var it = foo()
 
-console.log(x);
-it.next()
-console.log(x);
-bar()
-console.log(x);
-it.next()
-console.log(x);
+// console.log(x);
+// it.next()
+// console.log(x);
+// bar()
+// console.log(x);
+// it.next()
+// console.log(x);
+
+/** runner 简易版 */
+function run(gen) {
+  var args = [].slice.call(arguments, 1), it;
+  it = gen.apply(this, args);
+  return Promise.resolve().then(
+    function handleNext(value) {
+      var next = it.next(value);
+      return (function handleResult(next) {
+        if (next.done) {
+          return next.value
+        }
+        else {
+          return Promise.resolve(next.value).then(
+            handleNext,
+            function handleErr(err) {
+              return Promise.resolve(
+                it.throw(err)
+              ).then(handleResult)
+            }
+          )
+        }
+      }
+      )(next);
+    }
+  );
+}
