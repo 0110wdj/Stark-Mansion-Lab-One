5ce9d1daa7f9a452d556bf7e8c0da0600f5eb498
Snofly <49230243+0110wdj@users.noreply.github.com>
2023-10-19 20:58:11 +0800

    leetcode-1 两数之和基础解法

--- leetcode/1.两数之和.js
+++ leetcode/1.两数之和.js
@@ -10,8 +10,25 @@
  * @param {number} target
  * @return {number[]}
  */
-var twoSum = function(nums, target) {
-
+var twoSum = function (nums, target) {
+  // 1 sort nums to arr if use dichotomy
+  function mySort(nums) {
+    return [...nums]
+  }
+  const arr = mySort(nums)
+  // 2 find target from the rest of arr by dichotomy(or other ways)
+  let targetNum = 0
+  while (arr.length > 0) {
+    targetNum = target - arr.pop()
+    // const resIndex = myDichotomy(dichotomy, target)
+    const res = arr.find(i => i === targetNum)
+    if (res !== undefined) {
+      return [arr.indexOf(res), arr.length]
+    }
+  }
 };
 // @lc code=end
 
+// const nums = [2, 7, 11, 15]
+// const target = 9
+// console.log(twoSum(nums, target));
