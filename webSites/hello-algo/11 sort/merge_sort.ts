904eb0ad96c18d913141ba5def4a2c118563dda2
snofly <626796235@qq.com>
2024-03-01 00:17:58 +0800

    something

---
+++ webSites/hello-algo/11 sort/merge_sort.ts
@@ -0,0 +1,7 @@
+function mergeSort(nums: number[]): number[] {
+  function merge(nums: number[], ll: number, lr: number, rl: number, rr: number): void {
+  }
+  return nums
+}
+
+console.log(mergeSort([7, 3, 2, 6, 0, 1, 5, 4]));
