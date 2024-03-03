ef6d70329574b1268d87b3648b0cc2b874f314bf
Snofly <49230243+0110wdj@users.noreply.github.com>
2024-03-03 17:42:55 +0800

    permutations_ii_source

---
+++ webSites/hello-algo/13 backtracking/permutations_ii_source.ts
@@ -0,0 +1,36 @@
+/* 回溯算法：全排列 II */
+function backtrack(
+  state: number[],
+  choices: number[],
+  selected: boolean[],
+  res: number[][]
+): void {
+  // 当状态长度等于元素数量时，记录解
+  if (state.length === choices.length) {
+    res.push([...state]);
+    return;
+  }
+  // 遍历所有选择
+  const duplicated = new Set();
+  choices.forEach((choice, i) => {
+    // 剪枝：不允许重复选择元素 且 不允许重复选择相等元素
+    if (!selected[i] && !duplicated.has(choice)) {
+      // 尝试：做出选择，更新状态
+      duplicated.add(choice); // 记录选择过的元素值
+      selected[i] = true;
+      state.push(choice);
+      // 进行下一轮选择
+      backtrack(state, choices, selected, res);
+      // 回退：撤销选择，恢复到之前的状态
+      selected[i] = false;
+      state.pop();
+    }
+  });
+}
+
+/* 全排列 II */
+function permutationsII(nums: number[]): number[][] {
+  const res: number[][] = [];
+  backtrack([], nums, Array(nums.length).fill(false), res);
+  return res;
+}
