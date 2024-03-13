5e619f8ec6fb26b43bc16b4405c8c35bf2328038
Snofly <49230243+0110wdj@users.noreply.github.com>
2024-03-13 22:17:21 +0800

    eventloop

---
+++ OtherTest/JS/eventloop.js
@@ -0,0 +1,13 @@
+new Promise(() => {
+  console.log(1);
+  throw error
+  console.log(2);
+}).then(() => {
+  console.log(3);
+}, () => {
+  console.log(4);
+}).catch(() => {
+  console.log(5);
+}).then(() => {
+  console.log(6);
+})
