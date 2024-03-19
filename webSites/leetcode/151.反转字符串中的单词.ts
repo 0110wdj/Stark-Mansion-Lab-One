e159ac6d3b4cde94e7d52b4e5aee68745876ad2a
Snofly <49230243+0110wdj@users.noreply.github.com>
2024-03-19 20:50:37 +0800

    leetcode 151

---
+++ webSites/leetcode/151.反转字符串中的单词.ts
@@ -0,0 +1,15 @@
+/*
+ * @lc app=leetcode.cn id=151 lang=typescript
+ *
+ * [151] 反转字符串中的单词
+ */
+
+// @lc code=start
+function reverseWords(s: string): string {
+  return s.trim().split(/\s+/).reverse().join(' ')
+};
+// @lc code=end
+
+console.log(reverseWords("the sky is blue"));
+console.log(reverseWords("  hello world  "));
+console.log(reverseWords("a good   example"));
