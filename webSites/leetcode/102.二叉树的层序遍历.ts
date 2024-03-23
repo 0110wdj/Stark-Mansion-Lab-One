7bfc96dbd1291f245f0dee41e21d65dfda2a7a03
Snofly <49230243+0110wdj@users.noreply.github.com>
2024-03-23 14:42:37 +0800

    leetcode 103

--- webSites/leetcode/102.二叉树的层序遍历.ts
+++ webSites/leetcode/102.二叉树的层序遍历.ts
@@ -4,16 +4,16 @@
  * [102] 二叉树的层序遍历
  */
 
-class TreeNode {
-  val: number
-  left: TreeNode | null
-  right: TreeNode | null
-  constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
-    this.val = (val === undefined ? 0 : val)
-    this.left = (left === undefined ? null : left)
-    this.right = (right === undefined ? null : right)
-  }
-}
+// class TreeNode {
+//   val: number
+//   left: TreeNode | null
+//   right: TreeNode | null
+//   constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+//     this.val = (val === undefined ? 0 : val)
+//     this.left = (left === undefined ? null : left)
+//     this.right = (right === undefined ? null : right)
+//   }
+// }
 
 
 // @lc code=start
---
+++ webSites/leetcode/103.二叉树的锯齿形层序遍历.ts
@@ -0,0 +1,54 @@
+/*
+ * @lc app=leetcode.cn id=103 lang=typescript
+ *
+ * [103] 二叉树的锯齿形层序遍历
+ */
+
+class TreeNode {
+  val: number
+  left: TreeNode | null
+  right: TreeNode | null
+  constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+    this.val = (val === undefined ? 0 : val)
+    this.left = (left === undefined ? null : left)
+    this.right = (right === undefined ? null : right)
+  }
+}
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     val: number
+ *     left: TreeNode | null
+ *     right: TreeNode | null
+ *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.left = (left===undefined ? null : left)
+ *         this.right = (right===undefined ? null : right)
+ *     }
+ * }
+ */
+
+function zigzagLevelOrder(root: TreeNode | null): number[][] {
+  const res: number[][] = []
+  const recur = (head: TreeNode | null, deep: number): void => {
+    if (head === null) {
+      return
+    }
+    if (!res[deep]) { res[deep] = [] }
+    if (deep % 2 === 0) {
+      res[deep].push(head.val)
+    } else {
+      res[deep].unshift(head.val)
+    }
+    recur(head.left, deep + 1)
+    recur(head.right, deep + 1)
+  }
+  recur(root, 0)
+  return res
+};
+// @lc code=end
+
+console.log(zigzagLevelOrder({ val: 3, left: { val: 9, left: null, right: null }, right: { val: 20, left: { val: 15, left: null, right: null }, right: { val: 7, left: null, right: null } } }));
+console.log(zigzagLevelOrder({ val: 1, left: { val: 2, left: null, right: null }, right: { val: 3, left: { val: 4, left: null, right: null }, right: { val: 5, left: null, right: null } } }));
