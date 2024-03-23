b4a2ee210becfb943e1030f39126a1881497249f
Snofly <49230243+0110wdj@users.noreply.github.com>
2024-03-23 14:27:26 +0800

    leetcode 102

---
+++ webSites/leetcode/102.二叉树的层序遍历.ts
@@ -0,0 +1,50 @@
+/*
+ * @lc app=leetcode.cn id=102 lang=typescript
+ *
+ * [102] 二叉树的层序遍历
+ */
+
+class TreeNode {
+  val: number
+  left: TreeNode | null
+  right: TreeNode | null
+  constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+    this.val = (val === undefined ? 0 : val)
+    this.left = (left === undefined ? null : left)
+    this.right = (right === undefined ? null : right)
+  }
+}
+
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     val: number
+ *     left: TreeNode | null
+ *     right: TreeNode | null
+ *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.left = (left===undefined ? null : left)
+ *         this.right = (right===undefined ? null : right)
+ *     }
+ * }
+ */
+
+function levelOrder(root: TreeNode | null): number[][] {
+  const res: number[][] = []
+  const recur = (head: TreeNode | null, deep: number): void => {
+    if (head === null) {
+      return
+    }
+    if (!res[deep]) { res[deep] = [] }
+    res[deep].push(head.val)
+    recur(head.left, deep + 1)
+    recur(head.right, deep + 1)
+  }
+  recur(root, 0)
+  return res
+};
+// @lc code=end
+
+console.log(levelOrder({ val: 3, left: { val: 9, left: null, right: null }, right: { val: 20, left: { val: 15, left: null, right: null }, right: { val: 7, left: null, right: null } } }));
