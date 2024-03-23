e87f160a9b139b965a819bdfdaf7ddcbd01502f7
Snofly <49230243+0110wdj@users.noreply.github.com>
2024-03-23 17:50:20 +0800

    leetcode 215 quick sort

--- webSites/leetcode/215.数组中的第k个最大元素.ts
+++ webSites/leetcode/215.数组中的第k个最大元素.ts
@@ -7,40 +7,38 @@
 // 用堆排序 温习一下
 
 // @lc code=start
-
-function swap(arr: number[], i: number, j: number) {
-  [arr[i], arr[j]] = [arr[j], arr[i]]
-}
-
-function maxHeapify(a: number[], i: number, heapSize: number) {
-  let l = i * 2 + 1, r = i * 2 + 2, largest = i;
-  if (l < heapSize && a[l] > a[largest]) {
-    largest = l;
-  }
-  if (r < heapSize && a[r] > a[largest]) {
-    largest = r;
-  }
-  if (largest != i) {
-    swap(a, i, largest);
-    maxHeapify(a, largest, heapSize);
-  }
-}
-
-function buildMaxHeap(nums: number[], heapSize: number) {
-  for (let i = Math.ceil(heapSize / 2); i >= 0; i--) {
-    maxHeapify(nums, i, heapSize);
-  }
-}
-
 function findKthLargest(nums: number[], k: number): number {
-  let heapSize = nums.length;
-  buildMaxHeap(nums, heapSize);
-  for (let i = nums.length - 1; i >= nums.length - k + 1; --i) {
-    swap(nums, 0, i);
-    --heapSize;
-    maxHeapify(nums, 0, heapSize);
+  const len = nums.length;
+  const quickSort = (nums: number[], i: number, j: number) => {
+    let left = i + 1, right = j;
+    while (left <= right) {
+      if (nums[left] <= nums[i]) {
+        left++
+        continue
+      }
+      if (nums[right] >= nums[i]) {
+        right--
+        continue
+      }
+      [nums[left], nums[right]] = [nums[right], nums[left]]
+    }
+    [nums[i], nums[right]] = [nums[right], nums[i]]
+    if (right === len - k) {
+      return
+    }
+    if (right < len - k) {
+      quickSort(nums, right + 1, j)
+    } else {
+      quickSort(nums, i, right - 1)
+    }
   }
-  return nums[0];
+  quickSort(nums, 0, len - 1)
+  return nums.at(-k) ?? NaN;
 };
 
 // @lc code=end
+
+// console.log(findKthLargest([3, 2, 1, 5, 6, 4], 2));
+// console.log(findKthLargest([3, 2, 3, 1, 2, 4, 5, 5, 6], 4));
+console.log(findKthLargest([-1, 2, 0], 2));
+console.log(findKthLargest([1, 2, 3, 4, 5, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1], 5));
