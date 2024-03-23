e03ac499d25593872edcadf7c4f1a48e2ba13e8b
Snofly <49230243+0110wdj@users.noreply.github.com>
2024-03-23 15:54:41 +0800

    leetcode 230

---
+++ webSites/leetcode/230.二叉搜索树中第k小的元素.ts
@@ -0,0 +1,64 @@
+/*
+ * @lc app=leetcode.cn id=230 lang=typescript
+ *
+ * [230] 二叉搜索树中第K小的元素
+ */
+class TreeNode {
+  val: number
+  left: TreeNode | null
+  right: TreeNode | null
+  constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+    this.val = (val === undefined ? 0 : val)
+    this.left = (left === undefined ? null : left)
+    this.right = (right === undefined ? null : right)
+  }
+}
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     val: number
+ *     left: TreeNode | null
+ *     right: TreeNode | null
+ *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.left = (left===undefined ? null : left)
+ *         this.right = (right===undefined ? null : right)
+ *     }
+ * }
+ */
+
+function kthSmallest(root: TreeNode | null, k: number): number {
+  let res: number = 0
+  const recur = (root: TreeNode | null) => {
+    if (root === null) {
+      return null
+    }
+    recur(root.left)
+    if (k === 0) return null
+    if (--k === 0) { res = root.val }
+    recur(root.right)
+  }
+  recur(root)
+  return res
+};
+// @lc code=end
+
+const root: TreeNode = {
+  val: 3,
+  left: {
+    val: 1,
+    left: null,
+    right: {
+      val: 2,
+      left: null,
+      right: null
+    }
+  },
+  right: {
+    val: 4,
+    left: null,
+    right: null
+  }
+}
+console.log(kthSmallest(root, 1));
