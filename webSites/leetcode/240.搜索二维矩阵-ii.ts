99411fac6cdf661d5e4a803cf760c1a37c7e75bf
Snofly <49230243+0110wdj@users.noreply.github.com>
2024-03-26 20:49:11 +0800

    leetcode 240 newbee

--- webSites/leetcode/240.搜索二维矩阵-ii.ts
+++ webSites/leetcode/240.搜索二维矩阵-ii.ts
@@ -6,37 +6,12 @@
 
 // @lc code=start
 function searchMatrix(matrix: number[][], target: number): boolean {
-  // console.log({ matrix });
-
-  const m = matrix.length
-  if (m === 0) return false
-  const n = matrix[0].length
-  if (n === 0) return false
-  if (m <= 2 && n <= 2) {
-    for (let i = 0; i < m; i++) {
-      for (let j = 0; j < n; j++) {
-        if (matrix[i][j] === target) return true
-      }
-    }
-    return false
-  }
-  // 分成四份，递归
-  const mid_m = Math.floor(m / 2)
-  const mid_n = Math.floor(n / 2)
-  // console.log({ mid_m, mid_n });
-
-  if (matrix[mid_m][mid_n] === target) return true
-  if (matrix[mid_m][mid_n] > target) {
-    const rt = searchMatrix(matrix.slice(0, mid_m + 1).map(v => v.slice(mid_n + 1)), target);
-    const lb = searchMatrix(matrix.slice(mid_m + 1).map(v => v.slice(0, mid_n + 1)), target);
-    const ll = searchMatrix(matrix.slice(0, mid_m + 1).map(v => v.slice(0, mid_n + 1)), target)
-    return rt || lb || ll
-  }
-  if ((matrix[mid_m][mid_n] < target)) {
-    const rt = searchMatrix(matrix.slice(0, mid_m + 1).map(v => v.slice(mid_n + 1)), target);
-    const lb = searchMatrix(matrix.slice(mid_m + 1).map(v => v.slice(0, mid_n + 1)), target);
-    const rb = searchMatrix(matrix.slice(mid_m + 1).map(v => v.slice(mid_n + 1)), target);
-    return rt || lb || rb
+  let i = 0, j = matrix[0].length - 1;
+  while (j >= 0 && i < matrix.length) {
+    // console.log({ i, j, v: matrix[i][j] });
+    if (matrix[i][j] === target) return true;
+    if (matrix[i][j] > target) j--;
+    if (matrix[i][j] < target) i++;
   }
   return false
 };
