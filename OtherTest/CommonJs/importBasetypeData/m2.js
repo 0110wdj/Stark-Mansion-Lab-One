95587823e8f1dddfdb66cf1f3181ec9556e835f0
Snofly <49230243+0110wdj@users.noreply.github.com>
2024-04-12 17:41:56 +0800

    import request test

---
+++ OtherTest/CommonJs/importBasetypeData/m2.js
@@ -0,0 +1,3 @@
+import { foo } from './m1'
+console.log(foo);
+setTimeout(() => console.log(foo), 500)
---
+++ OtherTest/CommonJs/requireBasetypeData2/main.js
@@ -0,0 +1,4 @@
+const mod = require('./lib')
+console.log(mod.counter());
+mod.incCounter()
+console.log(mod.counter());
