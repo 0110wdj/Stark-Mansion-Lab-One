2b6403edcb40db7ec6400ade426f4027e09bac6a
Snofly <49230243+0110wdj@users.noreply.github.com>
2024-04-14 10:30:17 +0800

    test type var 等价

--- OtherTest/var/var.js
+++ OtherTest/var/var.js
@@ -1,7 +1,17 @@
+// let a = 1;
+// (function () {
+//   console.log(a);//undefined
+//   console.log(this.a);//undefined
+//   var a = '2';
+//   console.log(a + this.a);//2undefined
+// })();
+
+
 let a = 1;
-(function () {
-  console.log(a);//undefined
-  console.log(this.a);//undefined
-  var a = '2';
-  console.log(a + this.a);//2undefined
+; (function () {
+  var a;
+  console.log(a) //undefined
+  console.log(globalThis.a) //undefined
+  a = "2"
+  console.log(a + globalThis.a) //2undefined
 })();
