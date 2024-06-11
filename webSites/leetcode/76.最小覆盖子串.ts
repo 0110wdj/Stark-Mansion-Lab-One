5fc1f3a05bb4296ac9ce9065528dbc8ab51aece9
Snofly <49230243+0110wdj@users.noreply.github.com>
2024-06-11 17:19:39 +0800

    leetcode 76 source

--- webSites/leetcode/76.最小覆盖子串.ts
+++ webSites/leetcode/76.最小覆盖子串.ts
@@ -6,60 +6,50 @@
 
 // @lc code=start
 function minWindow(s: string, t: string): string {
-  if (s.length < t.length) return '';
-  let i = 0;
-  let j = 0;
-  let minLen = Number.MAX_VALUE;
-  let minStr = '';
-  const tarMap = new Map();
+  const m = s.length;
+  let ansLeft = -1;
+  let ansRight = m;
+  let left = 0;
+  let less = 0;
+  const cntS: number[] = Array(128).fill(0); // s 子串字母的出现次数
+  const cntT: number[] = Array(128).fill(0); // t 中字母的出现次数
   for (const c of t) {
-    tarMap.set(c, (tarMap.get(c) ?? 0) + 1)
-  }
-  for (const c of s) {
-    if (!tarMap.has(c)) { }
-  }
-
-  function checkGetAll(i: number, j: number): boolean {
-    const curMap = new Map();
-    const tarMap = new Map();
-    for (const c of t) {
-      tarMap.set(c, (tarMap.get(c) ?? 0) + 1)
+    if (cntT[c.codePointAt(0) ?? 0]++ === 0) {
+      less++; // 有 less 种字母的出现次数 < t 中的字母出现次数
     }
-    for (let k = i; k <= j; k++) {
-      const c = s.charAt(k)
-      if (tarMap.has(c)) {
-        curMap.set(c, (curMap.get(c) ?? 0) + 1)
+  }
+  for (let right = 0; right < m; right++) { // 移动子串右端点
+    const c = s[right].codePointAt(0) ?? 0; // 右端点字母（移入子串）
+    if (++cntS[c] === cntT[c]) {
+      less--; // c 的出现次数从 < 变成 >=
+    }
+    while (less === 0) { // 涵盖：所有字母的出现次数都是 >=
+      if (right - left < ansRight - ansLeft) { // 找到更短的子串
+        ansLeft = left; // 记录此时的左右端点
+        ansRight = right;
+      }
+      const x = s[left++].codePointAt(0) ?? 0; // 左端点字母（移出子串）
+      if (cntS[x]-- === cntT[x]) {
+        less++; // x 的出现次数从 >= 变成 <
       }
     }
-    // console.log(curMap, tarMap);
+  }
+  return ansLeft < 0 ? "" : s.substring(ansLeft, ansRight + 1);
+};
 
-    if (curMap.size !== tarMap.size) {
+function isCovered(cntS: number[], cntT: number[]) {
+  for (let i = 'A'.charCodeAt(0); i <= 'Z'.charCodeAt(0); i++) {
+    if (cntS[i] < cntT[i]) {
       return false;
     }
-    let flag = true;
-    curMap.forEach((value, key) => {
-      if (tarMap.get(key) > value) {
-        flag = false
-      }
-    });
-    return flag;
   }
-
-  while (j <= s.length) {
-    if (checkGetAll(i, j)) {
-      // console.log('inside', i, j);
-      if (j - i < minLen) {
-        minLen = j - i;
-        minStr = s.slice(i, j + 1)
-      }
-      i++;
-    } else {
-      // console.log('false:', i, j);
-      j++
+  for (let i = 'a'.charCodeAt(0); i <= 'z'.charCodeAt(0); i++) {
+    if (cntS[i] < cntT[i]) {
+      return false;
     }
   }
-  return minStr
-};
+  return true;
+}
 // @lc code=end
 
 console.log(minWindow("ADOBECODEBANC", "ABC"));
