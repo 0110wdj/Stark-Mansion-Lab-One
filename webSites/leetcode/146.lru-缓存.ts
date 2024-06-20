62e5e33a23f80cd51c1d1b3d9b1dcb48ce50ddca
Snofly <49230243+0110wdj@users.noreply.github.com>
2024-06-20 17:43:58 +0800

    leetcode 146 待完善

---
+++ webSites/leetcode/146.lru-缓存.ts
@@ -0,0 +1,113 @@
+/*
+ * @lc app=leetcode.cn id=146 lang=typescript
+ *
+ * [146] LRU 缓存
+ */
+
+class ListNode {
+    val: number
+    next: ListNode | null
+    key: number | null
+    pre: ListNode | null
+    constructor(val?: number, next?: ListNode | null, pre?: ListNode | null, key?: number | null,) {
+        this.val = (val === undefined ? -1 : val)
+        this.next = (next === undefined ? null : next)
+        this.pre = (pre === undefined ? null : pre)
+        this.key = (key === undefined ? null : key)
+    }
+}
+
+// @lc code=start
+
+class LRUCache {
+    head: ListNode | null = null;
+    last: ListNode | null = null;
+    nodeMap: Map<number, ListNode> = new Map();
+    constructor(capacity: number) {
+        for (let i = 0; i < capacity; i++) {
+            const newNode: ListNode = new ListNode()
+            if (i === 0) {
+                this.head = newNode
+                this.last = newNode
+            } else {
+                if (this.last) {
+                    this.last.next = newNode
+                    newNode.pre = this.last
+                    this.last = newNode
+                }
+            }
+        }
+    }
+
+    get(key: number): number {
+        return -1
+    }
+
+    put(key: number, value: number): void {
+        if (!this.nodeMap.has(key)) {
+            if (this.last === null) return
+            if (this.head === null) return
+            if (this.head.next === null) {
+                this.head.val = value
+                return
+            }
+            const newNode = new ListNode(value, null, this.last, key)
+            this.nodeMap.set(key, newNode)
+            this.last.next = newNode
+            if (this.head.key !== null) {
+                this.nodeMap.delete(this.head.key)
+            }
+            this.head = this.head.next
+            this.head.pre = null
+        } else {
+            const curNode = this.nodeMap.get(key) ?? null
+            if (curNode === null) return
+            if (curNode === this.head) {
+                if (this.last === null) return
+                if (this.head === null) return
+                if (this.head.next === null) {
+                    this.head.val = value
+                    return
+                }
+                this.head = this.head.next
+                this.head.pre = null
+                this.last.next = curNode
+                curNode.next = null
+                curNode.pre = this.last
+                this.last = curNode
+            } else {
+                if (this.last === null) return
+                if (this.head === null) return
+                if (curNode.pre === null) return
+                if (curNode.next === null) return
+                if (this.head.next === null) {
+                    this.head.val = value
+                    return
+                }
+                curNode.pre.next = curNode.next
+                curNode.next.pre = curNode.pre
+                this.last.next = curNode
+                curNode.next = null
+                curNode.pre = this.last
+                this.last = curNode
+            }
+        }
+    }
+}
+
+/**
+ * Your LRUCache object will be instantiated and called as such:
+ * var obj = new LRUCache(capacity)
+ * var param_1 = obj.get(key)
+ * obj.put(key,value)
+ */
+// @lc code=end
+
+const opt = new LRUCache(2)
+opt.put(1, 1)
+opt.put(2, 2)
+// opt.get(1)
+// opt.put(3, 3)
+console.log(opt);
+
+
