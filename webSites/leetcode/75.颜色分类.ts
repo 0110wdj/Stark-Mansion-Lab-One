b164dc9573fb129e029d8f0cb34b425df66c60c4
Snofly <49230243+0110wdj@users.noreply.github.com>
2024-06-29 13:29:12 +0800

    leetcode 75 source double points

--- webSites/leetcode/75.颜色分类.ts
+++ webSites/leetcode/75.颜色分类.ts
@@ -9,24 +9,32 @@
  Do not return anything, modify nums in-place instead.
  */
 function sortColors(nums: number[]): void {
-  const counts = [0, 0, 0];
-  for (const n of nums) {
-    counts[n]++;
-  }
-  for (let i = 0; i < nums.length; i++) {
-    let tmp = 1
-    if (i < counts[0]) {
-      tmp = 0
+  let p0 = 0;
+  let p1 = 0;
+  const n = nums.length;
+  for (let i = 0; i < n; ++i) {
+    if (nums[i] === 1) {
+      const temp = nums[i];
+      nums[i] = nums[p1];
+      nums[p1] = temp;
+      ++p1;
+    } else if (nums[i] === 0) {
+      let temp = nums[i];
+      nums[i] = nums[p0];
+      nums[p0] = temp;
+      if (p0 < p1) {
+        temp = nums[i];
+        nums[i] = nums[p1];
+        nums[p1] = temp;
+      }
+      ++p0;
+      ++p1;
     }
-    if (i >= counts[1] + counts[0]) {
-      tmp = 2
-    }
-    nums[i] = tmp
   }
 };
 // @lc code=end
 
-// const arr = [2, 0, 2, 1, 1, 0]
-const arr = [2, 0, 1]
+const arr = [2, 0, 2, 1, 1, 0]
+// const arr = [2, 0, 1]
 sortColors(arr)
 console.log(arr);
