9976efcbe3d61507673fbc1984f53c8cc4eacf56
Snofly <49230243+0110wdj@users.noreply.github.com>
2024-07-15 23:26:00 +0800

    treeclass test

--- OtherTest/ts/treeClass.ts
+++ OtherTest/ts/treeClass.ts
@@ -1,7 +1,7 @@
 // import { fetchArrayData } from '../api/someDataFetch'
 
 // 便于测试
-const fetchArrayData = async () => {
+const fetchArrayData = async (): Promise<TreeNode[]> => {
   return new Promise((resolve) => {
     setTimeout(() => {
       const returnData: TreeNode[] = [
@@ -38,7 +38,7 @@ const fetchArrayData = async () => {
           children: [],
         }
       ]
-      resolve('returnData')
+      resolve(returnData)
     }, 1000)
   })
 }
@@ -65,34 +65,71 @@ export interface TreeNode {
   // 祖先节点路径
   ancestors: string[]
   // 子结点
-  children: TreeNode[]
+  children: GuessMapContext[]
 }
 
-const arrToTree = (arr: TreeNode[]) => {
+const arrToTree = (arr: GuessArrayContext[]): GuessMapContext | null => {
   // 1.请完善该函数，将数组转换为树形结构
+  if (arr.length === 0) return null
+  const root: GuessMapContext = {
+    id: '',
+    children: [],
+  };
+  const map: Map<string, GuessMapContext> = new Map();
+  for (const item of arr) {
+    const { id, pid } = item;
+    const node: GuessMapContext = {
+      id,
+      children: [],
+    };
+    map.set(id, node);
+    if (pid === '') {
+      root.children.push(node);
+    } else {
+      const parent = map.get(pid);
+      if (parent) {
+        parent.children.push(node);
+      }
+    }
+  }
+  return root
 }
 
 // 按需取用
 type CreateParams = Pick<TreeNode, 'title' | 'pid'> & Partial<TreeNode>
 
+// 假设得到的 arr 是这样的
+type GuessArrayContext = Pick<TreeNode, 'id' | 'pid'> & Partial<TreeNode>
+
+type GuessMapContext = Pick<TreeNode, 'id' | 'children'> & Partial<TreeNode>
+
 export default class Tree {
-  treeData: TreeNode[]
-  dataMap: Record<string, boolean | number | string | TreeNode[] | string[]>
+  treeData: GuessArrayContext[];
+  dataMap: GuessMapContext | null;
   constructor() {
     this.treeData = []
-    this.dataMap = {}
+    this.dataMap = null
   }
 
-  // biome-ignore lint/suspicious/noExplicitAny: <explanation>
-  async fetchData(query: Record<string, any> = {}) {
-    const res = await fetchArrayData();
-    console.log({ res });
-    // this.dataMap = arrToTree(res)
-    // 从 fetchArrayData 接口获取数据
+  async fetchData(query: Record<string, unknown> = {}) {
     // 2. 完善该函数，要求：
     // 1）从 fetchArrayData 接口获取数据，通过 arrToTree 转换为树形结构，存储到 dataMap 中
     // 2）数据可以一次获取全部，数据量较大
+    const arr = await fetchArrayData();
+    if (this.dataMap === null) {
+      this.dataMap = arrToTree(arr);
+    } else {
+      // this.dataMap = arrToTree(arr);
+      // updateMap(this.dataMap, arr);
+    }
   }
-
   static modelName = 'some-tree-model'
 }
+
+// test code
+const tree = new Tree()
+console.log(tree);
+
+tree.fetchData().then(() => {
+  console.log(JSON.stringify(tree));
+})
