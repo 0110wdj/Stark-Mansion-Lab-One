7bf38602d98d025793444e3e8101f8ff404fa53e
Snofly <49230243+0110wdj@users.noreply.github.com>
2024-11-05 22:45:30 +0800

    leetcode 200

---
+++ webSites/leetcode/200.岛屿数量.java
@@ -0,0 +1,85 @@
+/*
+ * @lc app=leetcode.cn id=200 lang=java
+ *
+ * [200] 岛屿数量
+ */
+
+// @lc code=start
+class Solution {
+    void dfs(char[][] grid, int r, int c) {
+        int nr = grid.length;
+        int nc = grid[0].length;
+
+        if (r < 0 || c < 0 || r >= nr || c >= nc || grid[r][c] == '0') {
+            return;
+        }
+
+        grid[r][c] = '0';
+        dfs(grid, r - 1, c);
+        dfs(grid, r + 1, c);
+        dfs(grid, r, c - 1);
+        dfs(grid, r, c + 1);
+    }
+
+    public int numIslands(char[][] grid) {
+        if (grid == null || grid.length == 0) {
+            return 0;
+        }
+        int res = 0;
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[0].length; j++) {
+                if (grid[i][j] == '1') {
+                    res++;
+                    dfs(grid, i, j);
+                }
+            }
+        }
+        return res;
+    }
+}
+// @lc code=end
+
+class SolutionTest {
+    // 向上、下、左、右四个方向拓展，并将 1 全部变成 0，则是当前的岛屿
+    void dfs(char[][] grid, int r, int c) {
+        int nr = grid.length;
+        int nc = grid[0].length;
+
+        if (r < 0 || c < 0 || r >= nr || c >= nc || grid[r][c] == '0') {
+            return;
+        }
+
+        grid[r][c] = '0';
+        dfs(grid, r - 1, c);
+        dfs(grid, r + 1, c);
+        dfs(grid, r, c - 1);
+        dfs(grid, r, c + 1);
+    }
+
+    public int numIslands(char[][] grid) {
+        if (grid == null || grid.length == 0) {
+            return 0;
+        }
+        int res = 0;
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[0].length; j++) {
+                if (grid[i][j] == '1') {
+                    res++;
+                    dfs(grid, i, j);
+                }
+            }
+        }
+        return res;
+    }
+
+    public static void main(String[] args) {
+        char[][] grid = new char[4][5];
+        grid[0] = new char[] { '1', '1', '1', '1', '0' };
+        grid[1] = new char[] { '1', '1', '0', '1', '0' };
+        grid[2] = new char[] { '1', '1', '0', '0', '0' };
+        grid[3] = new char[] { '0', '0', '0', '0', '0' };
+        SolutionTest solution = new SolutionTest();
+        int res = solution.numIslands(grid);
+        System.out.println(res);
+    }
+}
