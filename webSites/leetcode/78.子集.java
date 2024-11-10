008b2942cd0e776c9be434bdc58fe482eb480361
Snofly <49230243+0110wdj@users.noreply.github.com>
2024-11-10 17:49:14 +0800

    leetcode 78 source

---
+++ webSites/leetcode/78.子集.java
@@ -0,0 +1,57 @@
+
+/*
+ * @lc app=leetcode.cn id=78 lang=java
+ *
+ * [78] 子集
+ */
+import java.util.ArrayList;
+import java.util.List;
+
+// @lc code=start
+class Solution {
+    List<Integer> t = new ArrayList<Integer>();
+    List<List<Integer>> ans = new ArrayList<List<Integer>>();
+
+    public List<List<Integer>> subsets(int[] nums) {
+        dfs(0, nums);
+        return ans;
+    }
+
+    public void dfs(int cur, int[] nums) {
+        if (cur == nums.length) {
+            ans.add(new ArrayList<Integer>(t));
+            return;
+        }
+        t.add(nums[cur]);
+        dfs(cur + 1, nums);
+        t.remove(t.size() - 1);
+        dfs(cur + 1, nums);
+    }
+}
+// @lc code=end
+
+class SolutionTest78 {
+    List<Integer> t = new ArrayList<Integer>();
+    List<List<Integer>> ans = new ArrayList<List<Integer>>();
+
+    public List<List<Integer>> subsets(int[] nums) {
+        dfs(0, nums);
+        return ans;
+    }
+
+    public void dfs(int cur, int[] nums) {
+        if (cur == nums.length) {
+            ans.add(new ArrayList<Integer>(t));
+            return;
+        }
+        t.add(nums[cur]);
+        dfs(cur + 1, nums);
+        t.remove(t.size() - 1);
+        dfs(cur + 1, nums);
+    }
+
+    public static void main(String[] args) {
+        int[] nums = { 1, 2, 3 };
+        System.out.println(new SolutionTest78().subsets(nums));
+    }
+}
