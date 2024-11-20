48d098eda284380946ce7dc1db30d4b8e9783a4e
Snofly <49230243+0110wdj@users.noreply.github.com>
2024-11-20 10:37:44 +0800

    leetcodeTop init

---
+++ webSites/leetcodeTop/leetcode/editor/cn/[3]无重复字符的最长子串.java
@@ -0,0 +1,15 @@
+// 3 无重复字符的最长子串
+
+//leetcode submit region begin(Prohibit modification and deletion)
+//class Solution {
+//    public int lengthOfLongestSubstring(String s) {
+//
+//    }
+//}
+//leetcode submit region end(Prohibit modification and deletion)
+
+class Solution {
+    public static void main(String[] args) {
+        System.out.println("leetcode top test");
+    }
+}
