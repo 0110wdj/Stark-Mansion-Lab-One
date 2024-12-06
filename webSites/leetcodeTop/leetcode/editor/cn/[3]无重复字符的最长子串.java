60b048a30ad92599437d8ee62b5f21ab6fe87564
Snofly <49230243+0110wdj@users.noreply.github.com>
2024-12-07 00:10:52 +0800

    leetcode 3 handle write

--- webSites/leetcodeTop/leetcode/editor/cn/[3]无重复字符的最长子串.java
+++ webSites/leetcodeTop/leetcode/editor/cn/[3]无重复字符的最长子串.java
@@ -36,20 +36,18 @@ import java.util.Set;
 class Solution {
     public static int lengthOfLongestSubstring(String s) {
         int len = s.length();
-        if (len == 0) return 0;
-        if (len == 1) return 1;
-
-        Set<Character> cur = new HashSet<>();
-        int left = 0, right = 0, maxLen = 0;
-        while (right < len) {
-            // 如果存在相同的值，则说明需要移动左侧了
-            if (cur.contains(s.charAt(right))) {
-                cur.remove(s.charAt(left));
-                left++;
+        if (len < 2) return len;
+        int maxLen = 0;
+        int i = 0, j = 0;
+        Set<Character> set = new HashSet<>();
+        while (j < len) {
+            if (set.contains(s.charAt(j))) {
+                set.remove(s.charAt(i));
+                i++;
             } else {
-                cur.add(s.charAt(right));
-                maxLen = Math.max(maxLen, right - left + 1);
-                right++;
+                set.add(s.charAt(j));
+                j++;
+                maxLen = Math.max(maxLen, j - i);
             }
         }
         return maxLen;
