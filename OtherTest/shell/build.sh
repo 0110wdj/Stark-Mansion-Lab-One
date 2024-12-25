8f96dbb25b55303453c2e7e6e45390aa02f808aa
Snofly <49230243+0110wdj@users.noreply.github.com>
2024-12-25 19:09:10 +0800

    jenkins shell config mv

--- OtherTest/shell/build.sh
+++ OtherTest/shell/build.sh
@@ -117,5 +117,23 @@ fi
 if $archive_desktop;then
 	./build-in-docker.sh --publish-desktop
 fi
+cd ./build/archive
+for file in *; do
+  if [ -f "$file" ]; then  # 确保是文件而非目录
+    # 处理 .tar.gz 文件
+    if [[ "$file" =~ \.tar\.gz$ ]]; then
+      new_name="${file%.tar.gz}-submodule-unstable.tar.gz"
+      mv "$file" "$new_name"
+    # 处理 .tar.xz 文件
+    elif [[ "$file" =~ \.tar\.xz$ ]]; then
+      new_name="${file%.tar.xz}-submodule-unstable.tar.xz"
+      mv "$file" "$new_name"
+    # 处理 .sh 文件
+    elif [[ "$file" =~ \.sh$ ]]; then
+      new_name="${file%.sh}-submodule-unstable.sh"
+      mv "$file" "$new_name"
+    fi
+  fi
+done
+cd ../../
 cd $WORKSPACE
-
--- OtherTest/shell/test.sh
+++ OtherTest/shell/test.sh
@@ -6,6 +6,23 @@ if [ -n "$ui_branch_cover" ]; then
     echo "not empty"
 fi
 
+
 cd ./dir
-ls
+for file in *; do
+  if [ -f "$file" ]; then  # 确保是文件而非目录
+    # 处理 .tar.gz 文件
+    if [[ "$file" =~ \.tar\.gz$ ]]; then
+      new_name="${file%.tar.gz}-submodule-unstable.tar.gz"
+      mv "$file" "$new_name"
+    # 处理 .tar.xz 文件
+    elif [[ "$file" =~ \.tar\.xz$ ]]; then
+      new_name="${file%.tar.xz}-submodule-unstable.tar.xz"
+      mv "$file" "$new_name"
+    # 处理 .sh 文件
+    elif [[ "$file" =~ \.sh$ ]]; then
+      new_name="${file%.sh}-submodule-unstable.sh"
+      mv "$file" "$new_name"
+    fi
+  fi
+done
 cd ../
