5ea62c5ce68508a953110212d1f19ed7538803cc
Snofly <49230243+0110wdj@users.noreply.github.com>
2025-01-03 22:20:49 +0800

    array 01-02

---
+++ webSites/Java-kuangstudy/JavaTest/src/cn/snofly/array/Demo1.java
@@ -0,0 +1,11 @@
+package cn.snofly.array;
+
+public class Demo1 {
+    public static void main(String[] args) {
+        int[] nums;
+        nums = new int[10];
+        for (int i = 0; i < nums.length; i++) {
+            System.out.println(nums[i]);
+        }
+    }
+}
