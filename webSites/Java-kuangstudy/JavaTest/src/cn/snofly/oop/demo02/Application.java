de4d8d6f428842c542d0ca93d9d75a1dfd6e162f
Snofly <49230243+0110wdj@users.noreply.github.com>
2025-01-22 22:13:47 +0800

    java oop 06

--- webSites/Java-kuangstudy/JavaTest/src/cn/snofly/oop/demo02/Application.java
+++ webSites/Java-kuangstudy/JavaTest/src/cn/snofly/oop/demo02/Application.java
@@ -1,8 +1,22 @@
 package cn.snofly.oop.demo02;
 
+import cn.snofly.oop.demo03.Pet;
+
 // 一个项目应该只有一个 main 方法
 public class Application {
     public static void main(String[] args) {
+        Pet dog = new Pet();
+        dog.name = "旺财";
+        dog.age = 3;
+        dog.shout();
+        System.out.println(dog.name);
+        System.out.println(dog.age);
 
+        Pet cat = new Pet();
+        cat.name = "猫";
+        cat.age = 3;
+        cat.shout();
+        System.out.println(cat.name);
+        System.out.println(cat.age);
     }
 }
---
+++ webSites/Java-kuangstudy/JavaTest/src/cn/snofly/oop/demo03/Pet.java
@@ -0,0 +1,11 @@
+package cn.snofly.oop.demo03;
+
+public class Pet {
+
+    public String name;
+    public int age;
+
+    public void shout() {
+        System.out.println("shout");
+    }
+}
