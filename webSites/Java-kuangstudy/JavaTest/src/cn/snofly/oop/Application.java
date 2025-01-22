c85a28a10b89d89197157911f44fbd6b980d6a2a
Snofly <49230243+0110wdj@users.noreply.github.com>
2025-01-22 22:15:12 +0800

    java oop 06 refactor

---
+++ webSites/Java-kuangstudy/JavaTest/src/cn/snofly/oop/Application.java
@@ -0,0 +1,8 @@
+package cn.snofly.oop;
+
+// 一个项目应该只有一个 main 方法
+public class Application {
+    public static void main(String[] args) {
+
+    }
+}
--- webSites/Java-kuangstudy/JavaTest/src/cn/snofly/oop/demo03/Pet.java
+++ webSites/Java-kuangstudy/JavaTest/src/cn/snofly/oop/demo03/Pet.java
@@ -9,3 +9,19 @@ public class Pet {
         System.out.println("shout");
     }
 }
+
+/**
+ * Pet dog = new Pet();
+ * dog.name = "旺财";
+ * dog.age = 3;
+ * dog.shout();
+ * System.out.println(dog.name);
+ * System.out.println(dog.age);
+ * <p>
+ * Pet cat = new Pet();
+ * cat.name = "猫";
+ * cat.age = 3;
+ * cat.shout();
+ * System.out.println(cat.name);
+ * System.out.println(cat.age);
+ */
