8cd3fbb289d5294d7219103da6a155ca8f94c47d
Snofly <49230243+0110wdj@users.noreply.github.com>
2025-02-20 16:07:14 +0800

    not used

--- webSites/leetcodeTop/leetcode/editor/cn/[70]爬楼梯.java
+++ webSites/leetcodeTop/leetcode/editor/cn/[70]爬楼梯.java
@@ -1,20 +1,20 @@
 // 70 爬楼梯
 
 //leetcode submit region begin(Prohibit modification and deletion)
-class Solution {
-    public int climbStairs(int n) {
-        if (n <= 2) return n;
-//        return climbStairs(n - 1) + climbStairs(n - 2);
-        int[] res = new int[]{1, 2};
-        int tmp = 1;
-        for (int i = 3; i <= n; i++) {
-            tmp = res[1];
-            res[1] = res[0] + res[1];
-            res[0] = tmp;
-        }
-        return res[1];
-    }
-}
+//class Solution {
+//    public int climbStairs(int n) {
+//        if (n <= 2) return n;
+////        return climbStairs(n - 1) + climbStairs(n - 2);
+//        int[] res = new int[]{1, 2};
+//        int tmp = 1;
+//        for (int i = 3; i <= n; i++) {
+//            tmp = res[1];
+//            res[1] = res[0] + res[1];
+//            res[0] = tmp;
+//        }
+//        return res[1];
+//    }
+//}
 //leetcode submit region end(Prohibit modification and deletion)
 
 class Solution70 {
