19214f1d365ba1598a3d4593e2c751f0a67589ba
Snofly <49230243+0110wdj@users.noreply.github.com>
2025-03-13 22:07:56 +0800

    leetcode 300 replace

--- webSites/leetcodeTop/leetcode/editor/cn/[300]最长递增子序列.java
+++ webSites/leetcodeTop/leetcode/editor/cn/[300]最长递增子序列.java
@@ -1,91 +1,80 @@
 // 300 最长递增子序列
 
 import java.util.Arrays;
+import java.util.Map;
 
 //leetcode submit region begin(Prohibit modification and deletion)
 //class Solution {
-//    public int lengthOfLIS(int[] nums) {
-//        int minLeft = 0;
-//        int max = 0;
-//        // 排除头部无用子序列
-//        for (int i = 1; i < nums.length; i++) {
-//            if (i == minLeft + 1 && nums[i] <= nums[minLeft]) {
-//                minLeft = i;
+//    static public void binarySearchAndReplace(int[] arr, int left, int right, int target) {
+//        while (left <= right) {
+//            int mid = left + (right - left) / 2;
+//            if (arr[mid] >= target) {
+//                right = mid - 1;
 //            } else {
-//                break;
+//                left = mid + 1;
 //            }
 //        }
-//        // 动态规划表格
-//        int[] dp = new int[nums.length];
-//        dp[minLeft] = 1;
+//        arr[left] = target;
+//    }
 //
-//        for (int i = minLeft; i < nums.length; i++) {
-//            int base = 1;
-//            for (int j = i - 1; j >= minLeft; j--) {
-//                if (nums[i] > nums[j]) {
-//                    base = Math.max(base, dp[j] + 1);
-//                }
+//    static public int lengthOfLIS(int[] nums) {
+//        if (nums.length <= 1) return nums.length;
+//
+//        int[] arr = new int[nums.length];
+//        arr[0] = nums[0];
+//        int last = 0;
+//
+//        for (int i = 1; i < nums.length; i++) {
+//            if (nums[i] > arr[last]) {
+//                last++;
+//                arr[last] = nums[i];
+//            } else if (nums[i] < arr[last]) {
+//                binarySearchAndReplace(arr, 0, last, nums[i]);
 //            }
-//            dp[i] = base;
-//            max = Arrays.stream(dp).max().getAsInt();
 //        }
-//        return max;
+//
+//        return last + 1;
 //    }
 //}
 //leetcode submit region end(Prohibit modification and deletion)
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-
 class Solution300 {
-    static public int lengthOfLIS(int[] nums) {
-        int minLeft = 0;
-        int max = 0;
-        // 排除头部无用子序列
-        for (int i = 1; i < nums.length; i++) {
-            if (i == minLeft + 1 && nums[i] <= nums[minLeft]) {
-                minLeft = i;
-            } else {
-                break;
-            }
-        }
-        // 动态规划表格
-        int[] dp = new int[nums.length];
-        dp[minLeft] = 1;
 
-        for (int i = minLeft; i < nums.length; i++) {
-            int base = 1;
-            for (int j = i - 1; j >= minLeft; j--) {
-                if (nums[i] > nums[j]) {
-                    base = Math.max(base, dp[j] + 1);
-                }
+    static public void binarySearchAndReplace(int[] arr, int left, int right, int target) {
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (arr[mid] >= target) {
+                right = mid - 1;
+            } else {
+                left = mid + 1;
             }
-            dp[i] = base;
-            max = Arrays.stream(dp).max().getAsInt();
         }
-        return max;
+        arr[left] = target;
+    }
+
+    static public int lengthOfLIS(int[] nums) {
+        if (nums.length <= 1) return nums.length;
+
+        int[] arr = new int[nums.length];
+        arr[0] = nums[0];
+        int last = 0;
+
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] > arr[last]) {
+                last++;
+                arr[last] = nums[i];
+            } else if (nums[i] < arr[last]) {
+                binarySearchAndReplace(arr, 0, last, nums[i]);
+            }
+        }
+
+        return last + 1;
     }
 
     public static void main(String[] args) {
         System.out.println(lengthOfLIS(new int[]{10, 9, 2, 5, 3, 7, 101, 18}) + "\t==4");
         System.out.println(lengthOfLIS(new int[]{0, 1, 0, 3, 2, 3}) + "\t==4");
         System.out.println(lengthOfLIS(new int[]{7, 7, 7, 7, 7, 7, 7}) + "\t==1");
+        System.out.println(lengthOfLIS(new int[]{3, 5, 6, 2, 5, 4, 19, 5, 6, 7, 12}) + "\t==6");
     }
 }
-
-//function lengthOfLIS(nums: number[]): number {
-//  const dp: number[] = [1]
-//    let max = 1
-//    for (let i = 1; i < nums.length; i++) {
-//        let base = 1
-//        for (let j = i - 1; j >= 0; j--) {
-//            if (nums[i] > nums[j]) {
-//                base = Math.max(base, dp[j] + 1)
-//            }
-//        }
-//        dp[i] = base
-//        max = Math.max(max, ...dp)
-//    }
-//    return max
-//};
