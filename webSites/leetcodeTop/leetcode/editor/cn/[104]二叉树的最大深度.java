200e2f862975e9db7fe16cdb64e1a9b5657e867b
Snofly <49230243+0110wdj@users.noreply.github.com>
2025-04-30 01:56:20 +0800

    leetcode 140

--- webSites/leetcodeTop/leetcode/editor/cn/[104]二叉树的最大深度.java
+++ webSites/leetcodeTop/leetcode/editor/cn/[104]二叉树的最大深度.java
@@ -17,10 +17,10 @@
  * }
  * }
  */
-class Solution {
-    public int maxDepth(TreeNode root) {
-        if (root == null) return 0;
-        return Math.max(maxDepth(root.right), maxDepth(root.left)) + 1;
-    }
-}
+//class Solution {
+//    public int maxDepth(TreeNode root) {
+//        if (root == null) return 0;
+//        return Math.max(maxDepth(root.right), maxDepth(root.left)) + 1;
+//    }
+//}
 //leetcode submit region end(Prohibit modification and deletion)
---
+++ webSites/leetcodeTop/leetcode/editor/cn/[LCR 140]训练计划 II.java
@@ -0,0 +1,29 @@
+// LCR 140 训练计划 II
+
+//leetcode submit region begin(Prohibit modification and deletion)
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ * int val;
+ * ListNode next;
+ * ListNode() {}
+ * ListNode(int val) { this.val = val; }
+ * ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+//class Solution {
+//    public ListNode trainingPlan(ListNode head, int cnt) {
+//        ListNode target = head;
+//        while (head != null) {
+//            if (cnt == 0) {
+//                target = target.next;
+//            } else {
+//                cnt--;
+//            }
+//            head = head.next;
+//        }
+//        return target;
+//    }
+//}
+//leetcode submit region end(Prohibit modification and deletion)
